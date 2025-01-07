7002887d2e51b16c214aca4eb3cfb5b2f02bf6f3
Beau Oster <osterbeau@gmail.com>
2025-01-07 13:15:57 -0600

    1/7/25-B.O-Added initial component and styline

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Header from './Header.tsx'
+import Entry from './entry.tsx'
 
 function App() {
 
   return (
     <>
-      <h1>Vite + React test</h1>
+    <Header />
+     <Entry />
     </>
   )
 }
