5e32135121fa5607a3cfc6d98aea091dcb99321e
Beau Oster <osterbeau@gmail.com>
2025-01-08 09:28:04 -0600

    1/8/25-B.O-Added props, cleaned up code

--- my-app/src/Entry.tsx
+++ my-app/src/Entry.tsx
@@ -1,18 +1,26 @@
-export default function Entry() {
+type EntryProps = {
+    img: string;
+    country: string;
+    map: string;
+    title: string;
+    date: string;
+    description: string;
+}
+
+
+export default function Entry(props: EntryProps) {
     return (
         <article className="journal-entry">
             <div className="main-image-container">
-                <img className="main-image" src="https://scrimba.com/links/travel-journal-japan-image-url" alt="mount fuji" />
+                <img className="main-image" src={props.img} alt="mount fuji" />
             </div>
             <div className="info-container">
                 <img className="marker" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRINfICFcdbosHp2VzHHDsPEXsoYI1cSYgxbZDyXnHDxEwzlyFW7djzh4eTskyJy-4uGTY&usqp=CAU" alt ="marker" />
-                <span className="country"> Japan </span>
-                <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on Google Maps</a>
-                <h2 className="entry-title">Mount Fuji</h2>
-                <p className="trip-dates">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className="entry-text">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). 
-                Mount Fuji is the single most popular tourist site in Japan, 
-                for both Japanese and foreign tourists.</p>
+                <span className="country">{props.country}</span>
+                <a href={props.map}>View on Google Maps</a>
+                <h2 className="entry-title">{props.title}</h2>
+                <p className="trip-dates">{props.date}</p>
+                <p className="entry-text">{props.description}</p>
             </div>
         </article>
     )
